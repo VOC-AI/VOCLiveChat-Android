5272b41a6b267b55f3f8f4fa9102a717d2756d10
daocaoren <511583046@qq.com>
2025-03-17 19:34:17 +0800

    update readme for lower agp.

--- app/src/main/java/com/rc/webcomponent/MainActivity.kt
+++ app/src/main/java/com/rc/webcomponent/MainActivity.kt
@@ -3,16 +3,16 @@ package com.rc.webcomponent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.vocai.sdk.Vocai
+import com.vocai.sdk.Vocai.Companion as VocaiSDK
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-        Vocai.Companion.getInstance().init(this,true)
+        VocaiSDK.instance.init(this,true)
         findViewById<TextView>(R.id.mChatStart).setOnClickListener {
-            Vocai.Companion.getInstance().startChat("19365","6731F71BE4B0187458389512", extra =  hashMapOf(
+            VocaiSDK.instance.startChat("19365","6731F71BE4B0187458389512", null,null,null,hashMapOf(
                 "email" to "boyuan.gao@shulex-tech.com"
             ))
         }
