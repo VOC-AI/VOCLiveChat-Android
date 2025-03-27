836c25084c06bbe68568c617aef4fa32b6819a78
daocaoren <511583046@qq.com>
2025-03-27 20:14:22 +0800

    update code to new lib version.

--- app/src/main/java/com/rc/webcomponent/MainActivity.kt
+++ app/src/main/java/com/rc/webcomponent/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.rc.webcomponent
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.vocai.sdk.Vocai.Companion as VocaiSDK
@@ -10,7 +11,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-        VocaiSDK.getInstance().init(this,true)
+        VocaiSDK.getInstance().init(this,true) {
+            Log.i("MainActivity","vocai sdk is canceled")
+        }
         findViewById<TextView>(R.id.mChatStart).setOnClickListener {
             VocaiSDK.getInstance().startChat("19365","6731F71BE4B0187458389512", null,null,null,hashMapOf(
                 "email" to "boyuan.gao@shulex-tech.com"
