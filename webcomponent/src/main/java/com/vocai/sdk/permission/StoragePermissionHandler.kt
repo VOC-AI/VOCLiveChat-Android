3f4d83fe26597e464a8cf7656eedd142ba90c4fc
zyq66 <edy@EDYdeMacBook-Pro.local>
2025-11-12 15:25:45 +0800

    buidl

--- webcomponent/src/main/java/com/vocai/sdk/permission/StoragePermissionHandler.kt
+++ webcomponent/src/main/java/com/vocai/sdk/permission/StoragePermissionHandler.kt
@@ -5,6 +5,7 @@ import android.os.Build
 import androidx.activity.result.ActivityResultCaller
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 
 class StoragePermissionHandler(
     val caller: ActivityResultCaller
@@ -28,15 +29,16 @@ class StoragePermissionHandler(
     }
 
     private fun getGalleryPermissionCompat(): Array<String> =
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            // Android 13+ 不需要权限，使用系统选择器
-            emptyArray()
-        } else {
-            getGalleryPermission()
-        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) getGalleryPermissionCompat33() else getGalleryPermission()
 
     private fun getGalleryPermission(): Array<String> = arrayOf(
         Manifest.permission.READ_EXTERNAL_STORAGE
+    )
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun getGalleryPermissionCompat33(): Array<String> = arrayOf(
+        Manifest.permission.READ_MEDIA_IMAGES,
+        Manifest.permission.READ_MEDIA_VIDEO
     )
 
 }
